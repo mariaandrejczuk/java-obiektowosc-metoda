39c3d6a3f838a5fd793a0acb74c274e9b215eade
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-01-14 14:53:56 +0100

    #1 realizacja zadania 1, przechowywanie info o kaloryferze, metoda void bezparametrowa

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         char[] red = {'r', 'e', 'd'};
         char[] blue = {'b', 'l', 'u', 'e'};
-        Radiator r1 = new Radiator(red, weight: 55, power: 1025);
-        Radiator r2 = new Radiator(blue, weight: 120, power: 5105);
-        Radiator r3 = new Radiator(red, weight: 15, power: 500);
+        Radiator r1 = new Radiator(red,  55, 1025);
+        Radiator r2 = new Radiator(blue,  120,  5105);
+        Radiator r3 = new Radiator(red,  15,  500);
 
         Radiator[] radiators = new Radiator[3];
         radiators[0] = r1;
@@ -12,13 +12,9 @@ public class Main {
         radiators[2] = r3;
 
         for(Radiator r : radiators){
-            System.out.println("Color: "+r.color);
-            for(char c : r.color){
-                System.out.println(c);
-            }
-            System.out.println();
-            System.out.println("Weight: "+r.weight);
-            System.out.println("Power: "+r.power);
+            r.display();
         }
+        radiators[1].weight = 119;
+        radiators[1].display();
     }
 }
--- src/Radiator.java
+++ src/Radiator.java
@@ -8,5 +8,15 @@ public class Radiator {
         this.weight = weight;
         this.power = power;
     }
+    void display(){
+        System.out.println("--------------------");
+        System.out.print("Color: ");
+        for(char c : color){
+            System.out.print(c);
+        }
+        System.out.println();
+        System.out.println("Weight: "+weight);
+        System.out.println("Power: "+power+"W");
+    }
 }
 
